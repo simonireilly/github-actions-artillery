d38a04a309b333e2e5a8593860f708bbaddc16b4
Simon Reilly <simon.reilly@infinityworks.com>
2021-04-18 10:05:35 +0100

    Test v0.5

--- src/markdown-report.ts
+++ src/markdown-report.ts
@@ -2,7 +2,9 @@
 export const reportToMarkdown = (report: Artillery.Report): string => {
 
   const reportLine = Object.values(report.aggregate.latency).join(` | `)
-
+  const codeLine = Object.entries(report.aggregate.codes).map(([key, value]) => {
+    return `| ${key} | ${value} | ${report.aggregate.requestsCompleted} | ${value/report.aggregate.requestsCompleted} |`
+  })
 
   return `
 ## Artillery Results
@@ -12,7 +14,13 @@ export const reportToMarkdown = (report: Artillery.Report): string => {
 - Ran ${report.aggregate.requestsCompleted}
 - Requests per seconds: ${report.aggregate.rps.mean} (req/s)
 
-| Requests | min (ms) | max (ms) | median (ms) | p95 (ms) | p99 (ms) |
+*Responses*
+| HTTP Code | Count | Total | Percentage |
+| --- | --- | --- | --- |
+
+
+*Latency*
+| Request Count | min (ms) | max (ms) | median (ms) | p95 (ms) | p99 (ms) |
 | -------- | -------- | -------- | ----------- | -------- | -------- |
 | ${report.aggregate.requestsCompleted} | ${reportLine} |
   `
