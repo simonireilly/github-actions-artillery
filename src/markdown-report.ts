8a52aeabd9dc17a10b99f76c32209fb78efd9eae
Simon Reilly <simon.reilly@infinityworks.com>
2021-04-18 09:53:27 +0100

    Test v0.1

--- src/markdown-report.ts
+++ src/markdown-report.ts
@@ -1,4 +1,12 @@
 
 export const reportToMarkdown = (report: Artillery.Report): string => {
   return JSON.stringify(report)
+
+  return `
+## Artillery Results
+
+| Requests |
+| --- |
+| ${report.aggregate.requestsCompleted}
+  `
 }
